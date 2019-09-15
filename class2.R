7339a63f73a2a35ed6cc95af95c8c6f447aac0f7
yeeun-grace <yeeun.lee@psych.ubc.ca>
2019-09-15 14:07:12 -0700

    new filenew file

---
+++ class2.R
@@ -0,0 +1,39 @@
+x<- 3
+x
+x <- c(1:10)  
+x
+x>8
+x>8 | x<5
+x[c(T,T,T,T,F,F,F,F,T,T)]
+times <- c(30:40)
+mean(times)
+range(times)
+sqrt(times)
+?sqrt
+times ==35
+times = 35
+times <- c(24,28,39,47,20,NA)
+mean(times)
+mean(times, na.rm=TRUE)
+times <- c(24,28,39,47,20)
+mean(times)
+times > 25
+times <- times[c(F,T,T,T,F,NA)]
+is.na(times)
+times
+...()
+na.rm(TRUE)
+mtcars
+head(mtcars)
+summary(mtcars)
+row.names(mtcars)
+names(mtcars)
+mtcars$cyl
+install.packages("tibble")
+install.packages("gapminder")
+library(tibble)
+library(gapminder)
+browseVignettes(package = "tibble")
+5+2
+'+'(5,2)
+?'{' 
